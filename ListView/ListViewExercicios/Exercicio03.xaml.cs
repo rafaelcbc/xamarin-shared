1d77182bcbefee5cadf1355ce4b6abb2bea28fd9
Rafael Cardoso <comerciorafael@hotmail.com>
2019-07-16 20:45:18 -0300

    Exercicio03 criado.

---
+++ ListView/ListViewExercicios/Exercicio03.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListViewExercicios
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Exercicio03 : ContentPage
+    {
+        public Exercicio03()
+        {
+            InitializeComponent();
+        }
+    }
+}
