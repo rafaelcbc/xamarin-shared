fd010fa7ae43217db1545ce9f9fa487fbe8f41fc
Rafael Cardoso <comerciorafael@hotmail.com>
2019-07-16 20:20:41 -0300

    Commit inical.

---
+++ ListViewExercicios/Exercicio01.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListViewExercicios
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Exercicio01 : ContentPage
+    {
+        public Exercicio01()
+        {
+            InitializeComponent();
+
+            var lstFrutas = new List<String>
+            {
+                "Abacate",
+                "Abacaxi",
+                "Banana",
+                "Caju",
+                "Figo",
+                "Goiaba",
+                "Laranja",
+                "Melancia",
+                "Pêssego",
+                "Tangerina"
+            };
+
+            lstView.ItemsSource = lstFrutas;
+        }
+
+        private void LstView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var frutas = e.SelectedItem as String;
+            DisplayAlert("Item Selecionado", frutas.ToString(), "OK");
+        }
+    }
+}
---
+++ ListViewExercicios/Exercicio02.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListViewExercicios
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Exercicio02 : ContentPage
+    {
+        public Exercicio02()
+        {
+            InitializeComponent();
+
+            var frutas = new List<String>
+            {
+                "Abacate",
+                "Abacaxi",
+                "Banana",
+                "Caju",
+                "Figo",
+                "Goiaba",
+                "Laranja",
+                "Melancia",
+                "Pêssego",
+                "Tangerina"
+            };
+
+            var listView = new ListView
+            {
+                ItemsSource = frutas
+            };
+
+            Content = listView;
+
+            listView.ItemSelected += (object sender, SelectedItemChangedEventArgs e) =>
+            {
+                var fruta = (String) e.SelectedItem;
+
+                DisplayAlert("Item Selecionado", fruta.ToString(), "OK");
+            };
+        }
+    }
+}
